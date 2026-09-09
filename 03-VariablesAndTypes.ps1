ef8e8c7c6e26fac633a9013fe6ac42c60ec4acce
Lily Xute <lily.xute@capgemini.com>
2026-09-09 15:17:13 +0100

    Add Variables and Output in PowerShell script

---
+++ 03-VariablesAndTypes.ps1
@@ -0,0 +1,15 @@
+[string]$name = "Lily"
+[int]$age = 30
+[bool]$isAdmin = $true
+
+$favouriteColours = @(
+    "Red"
+    "Blue"
+    "Green"
+)
+
+Write-Output $name
+Write-Output $age
+Write-Output $isAdmin
+
+$favouriteColours
